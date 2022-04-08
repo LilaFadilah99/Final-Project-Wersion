f2a43ec72c2a276035506f6ad4f2b54d8b8feaa6
Dila <lilafadila64@gmail.com>
2022-04-08 09:33:13 +0700

    fix error

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -38,69 +38,69 @@ function Register() {
 
   return (
     <>
-    <main class="d-flex min-vh-100 justify-content-center align-items-center" style={{backgroundColor: "#FFD365"}}>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-12 col-lg-12 col-xl-10 shadow rounded-6">
-            <div class="form row justify-content-center py-md-0 py-5" style={{backgroundColor: "white"}}>
-              <div class="text-center d-md-none">
+    <main className="d-flex min-vh-100 justify-content-center align-items-center" style={{backgroundColor: "#FFD365"}}>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-12 col-xl-10 shadow rounded-6">
+            <div className="form row justify-content-center py-md-0 py-5" style={{backgroundColor: "white"}}>
+              <div className="text-center d-md-none">
                 <h4>Complete Your Knowledge <span>by Reading Wersion</span></h4>
-                <p class="text-secondary mt-1">Let's get started</p>
+                <p className="text-secondary mt-1">Let's get started</p>
               </div>
-              <div class="col-lg-5 col-md-5 col-7 p-2 my-auto">
-                <img src={LoginPict} class="img-fluid w-100" alt="Signup Logo" />
+              <div className="col-lg-5 col-md-5 col-7 p-2 my-auto">
+                <img src={LoginPict} className="img-fluid w-100" alt="Signup Logo" />
               </div>
 
-              <div class="col-lg-6 col-md-6 col-12 p-lg-5 px-4 pe-md-2 py-md-5">
-                <div class="mb-4 d-none d-md-block">
+              <div className="col-lg-6 col-md-6 col-12 p-lg-5 px-4 pe-md-2 py-md-5">
+                <div className="mb-4 d-none d-md-block">
                   <h4>Complete Your Knowledge by<span> Reading Wesion</span></h4>
-                  <p class="text-secondary mt-1">Let's get started</p>
+                  <p className="text-secondary mt-1">Let's get started</p>
                 </div>
 
-                <div class="d-none alert alert-warning alert-dismissible fade show" role="alert">
+                <div className="d-none alert alert-warning alert-dismissible fade show" role="alert">
                   This email is already registered, please login
-                  <button type="button" class="btn-close"></button>
+                  <button type="button" className="btn-close"></button>
                 </div>
 
                 <form id="registForm" onSubmit= {handleRegister}>
-                  <div class="mb-3">
-                    <label for="name" class="form-label">Name</label>
-                    <div class="input-group">
-                      <span class="input-group-text" id="basic-addon1"><i class="far fa-user"></i></span>
-                      <input type="text" value={value.name} onChange={handleInput} name='name' class="form-control" placeholder="Enter your name" id="name" autocomplete="off" />
+                  <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <div className="input-group">
+                      <span className="input-group-text" id="basic-addon1"><i className="far fa-user"></i></span>
+                      <input type="text" value={value.name} onChange={handleInput} name='name' className="form-control" placeholder="Enter your name" id="name" autoComplete="off" />
                     </div>
-                    <small class="text-danger d-none">Please input your name</small>
+                    <small className="text-danger d-none">Please input your name</small>
                   </div>
 
-                  <div class="mb-3">
-                    <label for="email" class="form-label">E-mail</label>
-                    <div class="input-group">
-                      <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-at"></i></span>
-                      <input type="email" value={value.email} onChange={handleInput} name='email' class="form-control" placeholder="Enter your email" id="email" autocomplete="off" />
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label">E-mail</label>
+                    <div className="input-group">
+                      <span className="input-group-text" id="basic-addon1"><i className="fa-solid fa-at"></i></span>
+                      <input type="email" value={value.email} onChange={handleInput} name='email' className="form-control" placeholder="Enter your email" id="email" autoComplete="off" />
                     </div>
-                    <small class="text-danger d-none">Please enter a valid e-mail</small>
+                    <small className="text-danger d-none">Please enter a valid e-mail</small>
                   </div>
 
-                  <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <div class="input-group">
-                      <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-lock"></i></span>
-                      <input type="password" value={value.password} onChange={handleInput} name='password' class="form-control" placeholder="Enter your password" id="password" autocomplete="off" />
+                  <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <div className="input-group">
+                      <span className="input-group-text" id="basic-addon1"><i className="fa-solid fa-lock"></i></span>
+                      <input type="password" value={value.password} onChange={handleInput} name='password' className="form-control" placeholder="Enter your password" id="password" autoComplete="off" />
                     </div>
-                    <small class="text-secondary">At least 8 character, number and alphabet</small>
+                    <small className="text-secondary">At least 8 character, number and alphabet</small>
                   </div>
-                  <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="showPassword" />
-                    <label class="form-check-label" for="showPassword">Show password</label>
+                  <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="showPassword" />
+                    <label className="form-check-label" htmlFor="showPassword">Show password</label>
                   </div>
 
-                  <div class="d-grid col-12 mt-md-4 mt-4">
-                    <button type="submit" class="btn"  style={{backgroundColor: "#fdba12"}}>Sign Up</button>
+                  <div className="d-grid col-12 mt-md-4 mt-4">
+                    <button type="submit" className="btn"  style={{backgroundColor: "#fdba12"}}>Sign Up</button>
                   </div>
                 </form>
 
-                <div class="mt-4">
-                  <p class="mb-0">Already have an account? <Link to="/login" class="text-primary text-decoration-none">Login</Link></p>
+                <div className="mt-4">
+                  <p className="mb-0">Already have an account? <Link to="/login" className="text-primary text-decoration-none">Login</Link></p>
                 </div>
               </div>
             </div>
