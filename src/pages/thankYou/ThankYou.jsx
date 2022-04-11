423e3b0b6acbcb34105b17b60bce72ad4992fb3f
ella gia <ellagia22@gmail.com>
2022-04-11 23:15:25 +0800

    edit thanks you

--- src/pages/thankYou/ThankYou.jsx
+++ src/pages/thankYou/ThankYou.jsx
@@ -1,40 +1,49 @@
-import React from 'react'
-import '../thankYou/ThankYou.css'
-import Navbar from '../../components/navbar/Navbar'
-import DetailContack from '../../components/detailContack/DetailContack'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import "../thankYou/ThankYou.css";
+import Navbar from "../../components/navbar/Navbar";
+import DetailContack from "../../components/detailContack/DetailContack";
+import { useNavigate } from "react-router-dom";
 
 function ThankYou() {
   let navigate = useNavigate();
   return (
     <>
-    <Navbar/>
-       {/* Title */}
-       <section id='Title'>
-    <div className="container-fluid py-md-5 py-3">
-      <div className="container mt-5">
-        <div className="row text-start">
-          <div className="col-12">
-            <h1>Thanks for your Order....</h1>
+      <Navbar />
+      {/* Title */}
+      <section id="Title">
+        <div className="container-fluid py-md-5 py-3">
+          <div className="container mt-5">
+            <div className="row text-start">
+              <div className="col-12">
+                <h1>Thanks for your Order....</h1>
+              </div>
+            </div>
+            <div className="row py-5">
+              <div className="col-md-6 col-12">
+                <h3>Sekedar catatan singkat, kami telah menerima order Anda dan akan menghubungi Anda sesegera mungkin setelah anda menyelesaikan pembayaran.</h3>
+              </div>
+              <div className="col-md-6 col-12 mt-md-0 mt-3">
+                <p>
+                  Sementara itu mengapa Anda tidak mengecek media sosial kami dan testimonial klien kami yang sebenarnya untuk menunjukkan mengapa merek memilih untuk bekerja sama dengan kami. Atau kunjungi halaman tim kami untuk melihat
+                  wajah di balik email.
+                </p>
+                <button
+                  id="methodButton1"
+                  className="my-3"
+                  onClick={() => {
+                    navigate("/products");
+                  }}
+                >
+                  More Products
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="row py-5">
-            <div className="col-md-6 col-12">
-                <h3>Sekedar catatan singkat, kami telah menerima order Anda dan akan menghubungi Anda sesegera mungkin setelah anda menyelesaikan pembayaran.</h3>
-            </div>
-            <div className="col-md-6 col-12 mt-md-0 mt-3">
-                <p>Sementara itu mengapa Anda tidak mengecek media sosial kami dan testimonial klien kami yang sebenarnya untuk menunjukkan mengapa merek memilih untuk bekerja sama dengan kami. Atau kunjungi halaman tim kami untuk melihat wajah di balik email.</p>
-                <button id="methodButton1" className="my-3"  onClick={() => {navigate ('/products');}}>More Products</button> 
-            </div>
-            
-            
-        </div>
-      </div>
-    </div>
-    </section>
-    <DetailContack/>
+      </section>
+      <DetailContack />
     </>
-  )
+  );
 }
 
-export default ThankYou
+export default ThankYou;
